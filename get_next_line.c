cd18067c6b7e0a5e67340f2d3669e7871b173d0d
Boadu Djan <bdjan@c2s11d49.42wolfsburg.de>
2023-08-09 15:56:51 +0200

    pushing get_next_line

---
+++ get_next_line.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdjan <bdjan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/06 17:32:54 by bdjan             #+#    #+#             */
+/*   Updated: 2023/07/06 17:33:40 by bdjan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_rd_str(int fd, char *input_str)
+{
+	char	*buffer;
+	char	*temp;
+	int		rd_bytes;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(input_str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (rd_bytes == -1)
+			return (free(buffer), free(input_str), NULL);
+		buffer[rd_bytes] = '\0';
+		temp = ft_strjoin(input_str, buffer);
+		free(input_str);
+		input_str = temp;
+	}
+	return (free(buffer), input_str);
+}
+
+char	*ft_extract_line(char *input_str, int i)
+{
+	char	*str;
+
+	if (!input_str[i])
+		return (NULL);
+	while (input_str[i] && input_str[i] != '\n')
+		i++;
+	if (input_str[i] == '\n')
+		i++;
+	str = malloc(i + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (input_str[i] && input_str[i] != '\n')
+	{
+		str[i] = input_str[i];
+		i++;
+	}
+	if (input_str[i] == '\n')
+	{
+		str[i] = input_str[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_new_str(char *input_str)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (input_str[i] && input_str[i] != '\n')
+		i++;
+	if (!input_str[i])
+		return (free(input_str), NULL);
+	str = malloc((ft_strlen(input_str) - i + 1) * sizeof(char));
+	if (!str)
+		return (free(input_str), NULL);
+	i++;
+	j = 0;
+	while (input_str[i])
+		str[j++] = input_str[i++];
+	str[j] = '\0';
+	return (free(input_str), str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*input_str;
+	int			i;
+
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	if (!input_str)
+		input_str = ft_strdup("");
+	input_str = ft_rd_str(fd, input_str);
+	if (!input_str)
+		return (NULL);
+	line = ft_extract_line(input_str, i);
+	input_str = ft_new_str(input_str);
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdjan <bdjan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/14 11:45:04 by bdjan             #+#    #+#             */
+/*   Updated: 2023/07/14 11:55:18 by bdjan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char			*get_next_line(int fd);
+char			*ft_rd_str(int fd, char *input_str);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strjoin(const char *s1, const char *s2);
+size_t			ft_strlen(const char *s);
+char			*ft_extract_line(char *input_str, int i);
+char			*ft_new_str(char *input_str);
+char			*ft_strdup(const char *s1);
+
+#endif
